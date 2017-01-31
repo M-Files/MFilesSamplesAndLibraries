a7a69e5ce8941717bf41fe7c1ff331842818d229
CraigHawker <craig.hawker@gmail.com>
2017-01-31 11:50:25 +0000

    Added Chain Workflows sample Used to test how it appears on internal git server.

---
+++ Samples/ChainWorkflows/readme.md
@@ -0,0 +1,5 @@
+# Chain Workflows
+
+This sample shows how to move an object from one workflow to another once it reaches a specific state.
+
+This technique can be used to split complex workflows into smaller, more managable processes for staff.
---
+++ Samples/ChainWorkflows/src/ChainWorkflows/src/VaultApplication.cs
@@ -0,0 +1,66 @@
+﻿using MFiles.VAF;
+using MFiles.VAF.Common;
+using MFilesAPI;
+
+namespace ChainWorkflows
+{
+	public class Configuration
+	{
+		internal const string WorkflowOneAlias = "MFiles.Workflows.Workflow1";
+		internal const string WorkflowTwoAlias = "MFiles.Workflows.Workflow2";
+		internal const string WorkflowOneStateFinalAlias = "MFiles.WorkflowStates.Workflow1.Final";
+		internal const string WorkflowTwoStateInitialAlias = "MFiles.WorkflowStates.Workflow2.Initial";
+
+		[MFWorkflow(Required = false)]
+		public MFIdentifier WorkflowOne = WorkflowOneAlias;
+
+		[MFWorkflow(Required = false)]
+		public MFIdentifier WorkflowTwo = WorkflowTwoAlias;
+
+		[MFState(Required = false)]
+		public MFIdentifier WorkflowOneStateFinal = WorkflowOneStateFinalAlias;
+
+		[MFState(Required = false)]
+		public MFIdentifier WorkflowTwoStateInitial= WorkflowTwoStateInitialAlias;
+
+	}
+
+	/// <summary>
+	/// Simple vault application to demonstrate VAF.
+	/// </summary>
+	public class VaultApplication : VaultApplicationBase
+	{
+		[MFConfiguration("ChainWorkflows", "config")]
+		private Configuration config = new Configuration();
+
+
+
+		[StateAction(Configuration.WorkflowOneStateFinalAlias)]
+		public void HandleWorkflowOneFinalState(StateEnvironment env)
+		{
+			// Move it to the next workflow.
+			env.ObjVerEx.SetProperty((int) MFBuiltInPropertyDef.MFBuiltInPropertyDefWorkflow,
+				MFDataType.MFDatatypeLookup,
+				this.config.WorkflowTwo);
+
+			// Move it to the next state.
+			env.ObjVerEx.SetProperty((int) MFBuiltInPropertyDef.MFBuiltInPropertyDefState,
+				MFDataType.MFDatatypeLookup,
+				this.config.WorkflowTwoStateInitial);
+
+			// Save the properties.
+			env.ObjVerEx.SaveProperties();
+
+			// Ensure the last modified is correct.
+			var lastModifiedBy = new TypedValue();
+			lastModifiedBy.SetValue(MFDataType.MFDatatypeLookup, env.CurrentUserID);
+			env.Vault.ObjectPropertyOperations.SetLastModificationInfoAdmin(env.ObjVer,
+				UpdateLastModified: false,
+				UpdateLastModifiedBy: true,
+				LastModifiedBy: lastModifiedBy,
+				LastModifiedUtc: null);
+		}
+
+
+	}
+}
