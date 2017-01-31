16a83df5539d81222b6c05aa83e9a4bf9d322299
CraigHawker <craig.hawker@gmail.com>
2017-01-31 12:05:14 +0000

    Updated code comment and readability

--- Samples/ChainWorkflows/src/ChainWorkflows/src/VaultApplication.cs
+++ Samples/ChainWorkflows/src/ChainWorkflows/src/VaultApplication.cs
@@ -4,60 +4,55 @@ using MFilesAPI;
 
 namespace ChainWorkflows
 {
-	public class Configuration
-	{
-		internal const string WorkflowOneAlias = "MFiles.Workflows.Workflow1";
-		internal const string WorkflowTwoAlias = "MFiles.Workflows.Workflow2";
-		internal const string WorkflowOneStateFinalAlias = "MFiles.WorkflowStates.Workflow1.Final";
-		internal const string WorkflowTwoStateInitialAlias = "MFiles.WorkflowStates.Workflow2.Initial";
-
-		[MFWorkflow(Required = false)]
-		public MFIdentifier WorkflowOne = WorkflowOneAlias;
-
-		[MFWorkflow(Required = false)]
-		public MFIdentifier WorkflowTwo = WorkflowTwoAlias;
-
-		[MFState(Required = false)]
-		public MFIdentifier WorkflowOneStateFinal = WorkflowOneStateFinalAlias;
-
-		[MFState(Required = false)]
-		public MFIdentifier WorkflowTwoStateInitial= WorkflowTwoStateInitialAlias;
-
-	}
-
 	/// <summary>
-	/// Simple vault application to demonstrate VAF.
+	/// A vault application that moves objects which hit the end of one workflow
+	/// to the start of another workflow.
 	/// </summary>
-	public class VaultApplication : VaultApplicationBase
+	public class VaultApplication 
+		: VaultApplicationBase
 	{
+		/// <summary>
+		/// The running configuration, loaded from Name-Value storage.
+		/// </summary>
 		[MFConfiguration("ChainWorkflows", "config")]
-		private Configuration config = new Configuration();
-
-
-
+		private readonly Configuration config = new Configuration();
+		
+		/// <summary>
+		/// Handles when the end of the first workflow is hit,
+		/// updating the object and moving it to the start of the second workflow.
+		/// </summary>
+		/// <param name="env">The state environment.</param>
 		[StateAction(Configuration.WorkflowOneStateFinalAlias)]
 		public void HandleWorkflowOneFinalState(StateEnvironment env)
 		{
-			// Move it to the next workflow.
+			// Update the local collection of property values to
+			// move it to the next workflow.
+			// NOTE: SetProperty only updates the in-memory copy, not the server. The subsequent call to SaveProperties actually updates the object.
+			// NOTE: Using SaveProperty instead would update the version on the server immediately. Multiple calls to SaveProperty can slow your code down.
 			env.ObjVerEx.SetProperty((int) MFBuiltInPropertyDef.MFBuiltInPropertyDefWorkflow,
 				MFDataType.MFDatatypeLookup,
 				this.config.WorkflowTwo);
 
-			// Move it to the next state.
+			// Update the local collection of property values to
+			// move it to the correct workflow state.
+			// NOTE: SetProperty only updates the in-memory copy, not the server. The subsequent call to SaveProperties actually updates the object.
+			// NOTE: Using SaveProperty instead would update the version on the server immediately. Multiple calls to SaveProperty can slow your code down.
 			env.ObjVerEx.SetProperty((int) MFBuiltInPropertyDef.MFBuiltInPropertyDefState,
 				MFDataType.MFDatatypeLookup,
 				this.config.WorkflowTwoStateInitial);
 
 			// Save the properties.
+			// NOTE: Needed because of the SetProperty calls above.
 			env.ObjVerEx.SaveProperties();
 
 			// Ensure the last modified is correct.
+			// NOTE: If this was not called then the object would show last modifed by "(M-Files Server)".
 			var lastModifiedBy = new TypedValue();
 			lastModifiedBy.SetValue(MFDataType.MFDatatypeLookup, env.CurrentUserID);
 			env.Vault.ObjectPropertyOperations.SetLastModificationInfoAdmin(env.ObjVer,
-				UpdateLastModified: false,
 				UpdateLastModifiedBy: true,
 				LastModifiedBy: lastModifiedBy,
+				UpdateLastModified: false,
 				LastModifiedUtc: null);
 		}
 
