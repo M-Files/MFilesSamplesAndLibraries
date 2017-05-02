667010ff5f4dfbd431b75c335745bc56d6aea0ce
CraigHawker <craig.hawker@gmail.com>
2017-05-02 16:29:23 +0100

    Added ability for LookupPropertyValueSearchCondition to have multiple lookup Ids ("one of" search executed).

--- Libraries/MFaaP.MFWSClient/Searching/PropertyValues/LookupPropertyValueSearchCondition.cs
+++ Libraries/MFaaP.MFWSClient/Searching/PropertyValues/LookupPropertyValueSearchCondition.cs
@@ -10,15 +10,16 @@ namespace MFaaP.MFWSClient
 		/// Creates a <see cref="LookupPropertyValueSearchCondition"/>, searching for the lookup value supplied.
 		/// </summary>
 		/// <param name="propertyDefId">The Id of the property def to search by.</param>
-		/// <param name="lookupValueId">The Id of the lookup value to search by.</param>
+		/// <param name="lookupIds">The Ids of the lookup value to search by.</param>
 		/// <param name="searchConditionOperator">The operator to use (defaults to Equals if not provided).</param>
 		/// <param name="invertOperator">Whether to invert the search operator (defaults to false if not provided).</param>
+		/// <remarks>If multiple lookup Ids are present, then objects matching any one of the Ids will be returned (e.g. "class is one of 1, 2, 3, or 4").</remarks>
 		public LookupPropertyValueSearchCondition(
 			int propertyDefId,
-			int lookupValueId,
 			SearchConditionOperators searchConditionOperator = SearchConditionOperators.Equals,
-			bool invertOperator = false)
-			: base(propertyDefId, lookupValueId.ToString(), searchConditionOperator, invertOperator)
+			bool invertOperator = false,
+			params int[] lookupIds)
+			: base(propertyDefId, string.Join(",", lookupIds), searchConditionOperator, invertOperator)
 		{
 		}
 
@@ -28,15 +29,16 @@ namespace MFaaP.MFWSClient
 		/// loaded from an external system: http://www.m-files.com/user-guide/latest/eng/#Connection_to_external_database.html.
 		/// </summary>
 		/// <param name="propertyDefId">The Id of the property def to search by.</param>
-		/// <param name="externalLookupValueId">The Id of the lookup value IN THE EXTERNAL SYSTEM.</param>
+		/// <param name="externalLookupIds">The Ids of the lookup value IN THE EXTERNAL SYSTEM.</param>
 		/// <param name="searchConditionOperator">The operator to use (defaults to Equals if not provided).</param>
 		/// <param name="invertOperator">Whether to invert the search operator (defaults to false if not provided).</param>
+		/// <remarks>If multiple lookup Ids are present, then objects matching any one of the Ids will be returned (e.g. "class is one of 1, 2, 3, or 4").</remarks>
 		public LookupPropertyValueSearchCondition(
 			int propertyDefId,
-			string externalLookupValueId,
 			SearchConditionOperators searchConditionOperator = SearchConditionOperators.Equals,
-			bool invertOperator = false)
-			: base(propertyDefId, "e" + externalLookupValueId, searchConditionOperator, invertOperator)
+			bool invertOperator = false,
+			params string[] externalLookupIds)
+			: base(propertyDefId, "e" + string.Join(",e", externalLookupIds), searchConditionOperator, invertOperator)
 		{
 		}
 	}
--- Libraries/MFaaP.MFWSClient/Searching/PropertyValues/MultiSelectLookupPropertyValueSearchCondition.cs
+++ Libraries/MFaaP.MFWSClient/Searching/PropertyValues/MultiSelectLookupPropertyValueSearchCondition.cs
@@ -7,23 +7,24 @@ namespace MFaaP.MFWSClient
 		: PropertyValueSearchConditionBase<string>
 	{
 		/// <summary>
-		/// Creates a <see cref="LookupPropertyValueSearchCondition"/>, searching for the lookup value supplied.
+		/// Creates a <see cref="MultiSelectLookupPropertyValueSearchCondition"/>, searching for the lookup value supplied.
 		/// </summary>
 		/// <param name="propertyDefId">The Id of the property def to search by.</param>
 		/// <param name="lookupIds">The Ids of the lookup value to search by.</param>
 		/// <param name="searchConditionOperator">The operator to use (defaults to Equals if not provided).</param>
 		/// <param name="invertOperator">Whether to invert the search operator (defaults to false if not provided).</param>
+		/// <remarks>If multiple lookup Ids are present, then objects matching any one of the Ids will be returned (e.g. "class is one of 1, 2, 3, or 4").</remarks>
 		public MultiSelectLookupPropertyValueSearchCondition(
 			int propertyDefId,
-			int[] lookupIds,
 			SearchConditionOperators searchConditionOperator = SearchConditionOperators.Equals,
-			bool invertOperator = false)
+			bool invertOperator = false,
+			params int[] lookupIds)
 			: base(propertyDefId, string.Join(",", lookupIds), searchConditionOperator, invertOperator)
 		{
 		}
 
 		/// <summary>
-		/// Creates a <see cref="LookupPropertyValueSearchCondition"/>, searching for the lookup value supplied.
+		/// Creates a <see cref="MultiSelectLookupPropertyValueSearchCondition"/>, searching for the lookup value supplied.
 		/// Note that this constructor allows you to provide an external Id (or display Id).  This is used ONLY when objects are
 		/// loaded from an external system: http://www.m-files.com/user-guide/latest/eng/#Connection_to_external_database.html.
 		/// </summary>
@@ -31,11 +32,12 @@ namespace MFaaP.MFWSClient
 		/// <param name="externalLookupIds">The Ids of the lookup value IN THE EXTERNAL SYSTEM.</param>
 		/// <param name="searchConditionOperator">The operator to use (defaults to Equals if not provided).</param>
 		/// <param name="invertOperator">Whether to invert the search operator (defaults to false if not provided).</param>
+		/// <remarks>If multiple lookup Ids are present, then objects matching any one of the Ids will be returned (e.g. "class is one of 1, 2, 3, or 4").</remarks>
 		public MultiSelectLookupPropertyValueSearchCondition(
 			int propertyDefId,
-			string[] externalLookupIds,
 			SearchConditionOperators searchConditionOperator = SearchConditionOperators.Equals,
-			bool invertOperator = false)
+			bool invertOperator = false,
+			params string[] externalLookupIds)
 			: base(propertyDefId, "e" + string.Join(",e", externalLookupIds), searchConditionOperator, invertOperator)
 		{
 		}
