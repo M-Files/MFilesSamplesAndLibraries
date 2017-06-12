1f68e81c2e64d3ce2fe782167a73696638e21ea3
CraigHawker <craig.hawker@gmail.com>
2017-06-12 09:35:30 +0100

    Added unit tests for setting single object property.

--- Libraries/MFaaP.MFWSClient.Tests/MFWSVaultObjectPropertyOperations.cs
+++ Libraries/MFaaP.MFWSClient.Tests/MFWSVaultObjectPropertyOperations.cs
@@ -3,8 +3,10 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MFaaP.MFWSClient.Tests.ExtensionMethods;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace MFaaP.MFWSClient.Tests
@@ -261,6 +263,194 @@ namespace MFaaP.MFWSClient.Tests
 
 			// Method must be correct.
 			Assert.AreEqual(Method.GET, methodUsed);
+		}
+
+		#endregion
+
+		#region SetProperties
+
+		/// <summary>
+		/// Ensures that a call to <see cref="MFaaP.MFWSClient.MFWSVaultObjectPropertyOperations.SetProperty(int,int,MFaaP.MFWSClient.PropertyValue,System.Nullable{int},System.Threading.CancellationToken)"/>
+		/// requests the correct resource address.
+		/// </summary>
+		[TestMethod]
+		public void SetProperty_CorrectResource()
+		{
+			/* Arrange */
+
+			// The actual requested address.
+			var resourceAddress = "";
+
+			// Create our restsharp mock.
+			var mock = new Mock<IRestClient>();
+
+			// When the execute method is called, log the resource requested.
+			mock
+				.Setup(c => c.ExecuteTaskAsync<ExtendedObjectVersion>(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()))
+				.Callback((IRestRequest r, CancellationToken t) => {
+					resourceAddress = r.Resource;
+				})
+				// Return a mock response.
+				.Returns(() =>
+				{
+					// Create the mock response.
+					var response = new Mock<IRestResponse<ExtendedObjectVersion>>();
+
+					// Setup the return data.
+					response.SetupGet(r => r.Data)
+						.Returns(new ExtendedObjectVersion());
+
+					//Return the mock object.
+					return Task.FromResult(response.Object);
+				});
+
+			/* Act */
+
+			// Create our MFWSClient.
+			var mfwsClient = MFWSClient.GetMFWSClient(mock);
+
+			// Execute.
+			mfwsClient.ObjectPropertyOperations.SetProperty(1, 2, new PropertyValue()
+			{
+				PropertyDef = 0,
+				TypedValue = new TypedValue()
+				{
+					DataType = MFDataType.Text,
+					Value = "hello world"
+				}
+			});
+
+			/* Assert */
+
+			// Execute must be called once.
+			mock.Verify(c => c.ExecuteTaskAsync<ExtendedObjectVersion>(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()), Times.Exactly(1));
+
+			// Resource must be correct.
+			Assert.AreEqual("/REST/objects/1/2/latest/properties/0", resourceAddress);
+		}
+
+		/// <summary>
+		/// Ensures that a call to <see cref="MFaaP.MFWSClient.MFWSVaultObjectPropertyOperations.SetProperty(int,int,MFaaP.MFWSClient.PropertyValue,System.Nullable{int},System.Threading.CancellationToken)"/>
+		/// uses the correct Http method.
+		/// </summary>
+		[TestMethod]
+		public void SetProperty_CorrectMethod()
+		{
+			/* Arrange */
+
+			// The method.
+			Method? methodUsed = null;
+
+			// Create our restsharp mock.
+			var mock = new Mock<IRestClient>();
+
+			// When the execute method is called, log the resource requested.
+			mock
+				.Setup(c => c.ExecuteTaskAsync<ExtendedObjectVersion>(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()))
+				.Callback((IRestRequest r, CancellationToken t) => {
+					methodUsed = r.Method;
+				})
+				// Return a mock response.
+				.Returns(() =>
+				{
+					// Create the mock response.
+					var response = new Mock<IRestResponse<ExtendedObjectVersion>>();
+
+					// Setup the return data.
+					response.SetupGet(r => r.Data)
+						.Returns(new ExtendedObjectVersion());
+
+					//Return the mock object.
+					return Task.FromResult(response.Object);
+				});
+
+			/* Act */
+
+			// Create our MFWSClient.
+			var mfwsClient = MFWSClient.GetMFWSClient(mock);
+
+			// Execute.
+			mfwsClient.ObjectPropertyOperations.SetProperty(1, 2, new PropertyValue()
+			{
+				PropertyDef = 0,
+				TypedValue = new TypedValue()
+				{
+					DataType = MFDataType.Text,
+					Value = "hello world"
+				}
+			});
+
+			/* Assert */
+
+			// Execute must be called once.
+			mock.Verify(c => c.ExecuteTaskAsync<ExtendedObjectVersion>(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()), Times.Exactly(1));
+
+			// Method must be correct.
+			Assert.AreEqual(Method.PUT, methodUsed);
+		}
+
+		/// <summary>
+		/// Ensures that a call to <see cref="MFaaP.MFWSClient.MFWSVaultObjectPropertyOperations.SetProperty(int,int,MFaaP.MFWSClient.PropertyValue,System.Nullable{int},System.Threading.CancellationToken)"/>
+		/// uses the correct Http method.
+		/// </summary>
+		[TestMethod]
+		public void SetProperty_CorrectBody()
+		{
+			/* Arrange */
+
+			// The method.
+			PropertyValue body = null;
+
+			// Create our restsharp mock.
+			var mock = new Mock<IRestClient>();
+
+			// When the execute method is called, log the resource requested.
+			mock
+				.Setup(c => c.ExecuteTaskAsync<ExtendedObjectVersion>(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()))
+				.Callback((IRestRequest r, CancellationToken t) => {
+					body = r.DeserializeBody<PropertyValue>();
+				})
+				// Return a mock response.
+				.Returns(() =>
+				{
+					// Create the mock response.
+					var response = new Mock<IRestResponse<ExtendedObjectVersion>>();
+
+					// Setup the return data.
+					response.SetupGet(r => r.Data)
+						.Returns(new ExtendedObjectVersion());
+
+					//Return the mock object.
+					return Task.FromResult(response.Object);
+				});
+
+			/* Act */
+
+			// Create our MFWSClient.
+			var mfwsClient = MFWSClient.GetMFWSClient(mock);
+
+			// Execute.
+			mfwsClient.ObjectPropertyOperations.SetProperty(1, 2, new PropertyValue()
+			{
+				PropertyDef = 0,
+				TypedValue = new TypedValue()
+				{
+					DataType = MFDataType.Text,
+					Value = "hello world"
+				}
+			});
+
+			/* Assert */
+
+			// Execute must be called once.
+			mock.Verify(c => c.ExecuteTaskAsync<ExtendedObjectVersion>(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()), Times.Exactly(1));
+
+			// Body must be correct.
+			Assert.IsNotNull(body);
+			Assert.AreEqual(0, body.PropertyDef);
+			Assert.IsNotNull(body.TypedValue);
+			Assert.AreEqual(MFDataType.Text, body.TypedValue.DataType);
+			Assert.AreEqual("hello world", body.TypedValue.Value);
 		}
 
 		#endregion
