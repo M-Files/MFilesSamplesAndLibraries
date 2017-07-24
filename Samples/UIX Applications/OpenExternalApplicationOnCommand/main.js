0da611bdb3da7547961146460dedcc7a0ca3a0ea
CraigHawker <craig.hawker@gmail.com>
2017-07-24 09:37:18 +0100

    Removed global-scope variables and improved code formatting.

--- Samples/UIX Applications/OpenExternalApplicationOnCommand/main.js
+++ Samples/UIX Applications/OpenExternalApplicationOnCommand/main.js
@@ -4,105 +4,116 @@
 
 "use strict";
 
-// Hold a reference to our command button (populated in getShellFrameStartedHandler).
-var g_MyCommandId = null;
-
-// Hold a reference to the selected items (populated in getNewShellListingHandler).
-var g_SelectedItems = null;
-
-function OnNewShellUI(shellUI) {
+function OnNewShellUI(shellUI)
+{
 	/// <summary>The entry point of ShellUI module.</summary>
 	/// <param name="shellUI" type="MFiles.ShellUI">The new shell UI object.</param>
 
-	// Register to listen new shell frame creation event.
-	try {
-		shellUI.Events.Register( Event_NewShellFrame, newShellFrameHandler );
+	// This is the start point of a ShellUI module.
+
+	// Register to be notified when a new shell frame (Event_NewShellFrame) is created.
+	try
+	{
+		shellUI.Events.Register(
+			Event_NewShellFrame,
+			handleNewShellFrame );
 	}
-	catch (e) {} // Suppress any "not ready" exceptions.
+	catch (e)
+	{} // Suppress any "not ready" exceptions.
 }
 
-function newShellFrameHandler(shellFrame) {
+function handleNewShellFrame(shellFrame)
+{
 	/// <summary>Handles the OnNewShellFrame event.</summary>
 	/// <param name="shellFrame" type="MFiles.ShellFrame">The new shell frame object.</param>
 
-	try {
+	try
+	{
 		// Register to be notified when the shell frame is started.
 		// Note: the shell frame may not be usable yet.
-		shellFrame.Events.Register( Event_Started, getShellFrameStartedHandler( shellFrame ) );
-
-		// Register to be notified when a new shell listing is created.
-		// Note: we don't actually need this here, but often the application needs to be
-		// passed details about the selected object(s), so useful to have this stub here.
-		shellFrame.Events.Register( Event_NewShellListing, getNewShellListingHandler( shellFrame ) );
+		shellFrame.Events.Register(
+			Event_Started,
+			getShellFrameStartedHandler( shellFrame ) );
 	}
-	catch (e) {} // Suppress any "not ready" exceptions.
+	catch (e)
+	{} // Suppress any "not ready" exceptions.
 }
 
-function getShellFrameStartedHandler(shellFrame) {
+function getShellFrameStartedHandler(shellFrame)
+{
 	/// <summary>Gets a function to handle the Started event for shell frame.</summary>
 	/// <param name="shellFrame" type="MFiles.ShellFrame">The current shell frame object.</param>
 	/// <returns type="MFiles.Events.OnStarted">The event handler.</returns>
-	return function() {
 
-		g_MyCommandId = shellFrame.Commands.CreateCustomCommand( "Open Application" );
+	return function()
+	{
+		var commandId = shellFrame.Commands.CreateCustomCommand( "Open Application" );
 
 		// Add it to the context menu.
 		// ref: http://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ICommands~AddCustomCommandToMenu.html
-		shellFrame.Commands.AddCustomCommandToMenu( g_MyCommandId, MenuLocation_ContextMenu_Top, 0 );
+		shellFrame.Commands.AddCustomCommandToMenu( commandId, MenuLocation_ContextMenu_Top, 0 );
 
-		try {
+		try
+		{
 			// Add it to the task pane.
 			// ref: http://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ITaskPane~AddCustomCommandToGroup.html
-			shellFrame.TaskPane.AddCustomCommandToGroup( g_MyCommandId, TaskPaneGroup_Main, 0 );
+			shellFrame.TaskPane.AddCustomCommandToGroup( commandId, TaskPaneGroup_Main, 0 );
 		}
-		catch (e) {
+		catch (e)
+		{
 			// This excepts inside the history view, as the task pane cannot be accessed.
 		}
 
 		// Register to be notified when someone clicks the button.
-		shellFrame.Commands.Events.Register( Event_CustomCommand,
-			function(command) {
-
+		shellFrame.Commands.Events.Register(
+			Event_CustomCommand,
+			function(command)
+			{
 				// We only care about our command.
-				if (command != g_MyCommandId) {
+				if (command != commandId)
+				{
 					return;
 				}
 
 				// Open the application.
 				var shell = new ActiveXObject( "WScript.Shell" );
 				shell.Run( "notepad.exe" );
-
 			} );
 
 		// Hide the command everywhere (affects both context menu and task pane) by default.
 		// ref: http://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ICommands~SetCommandState.html
-		shellFrame.Commands.SetCommandState( g_MyCommandId, CommandLocation_All, CommandState_Hidden );
+		shellFrame.Commands.SetCommandState( commandId, CommandLocation_All, CommandState_Hidden );
 
+		// Register to be notified when a new shell listing is created.
+		// Note: we don't actually need this here, but often the application needs to be
+		// passed details about the selected object(s), so useful to have this stub here.
+		shellFrame.Events.Register( Event_NewShellListing, getNewShellListingHandler( shellFrame, commandId ) );
 	};
 }
 
-function getNewShellListingHandler(shellFrame) {
+function getNewShellListingHandler(shellFrame, commandId)
+{
 	/// <summary>Gets a function to handle the NewShellListing event for shell frame.</summary>
 	/// <param name="shellFrame" type="MFiles.ShellFrame">The current shell frame object.</param>
 	/// <returns type="MFiles.Events.OnNewShellListing">The event handler.</returns>
 
 	// Return the handler function for NewShellListing event.
-	return function(shellListing) {
-
+	return function(shellListing)
+	{
 		// Listen for selection change events on the listing
-		shellListing.Events.Register( Event_SelectionChanged,
-			function(selectedItems) {
-
+		shellListing.Events.Register(
+			Event_SelectionChanged,
+			function(selectedItems)
+			{
 				// We may get many events raised; only deal with the active listing.
-				if (false == shellListing.IsActive) {
+				if (false == shellListing.IsActive)
+				{
 					return;
 				}
 
-				// Store the selected items.
-				g_SelectedItems = selectedItems;
-
 				// Sanity.
-				if (null == g_MyCommandId) {
+				if (null == commandId)
+				{
 					return;
 				}
 
@@ -111,21 +122,18 @@ function getNewShellListingHandler(shellFrame) {
 				var commandViable = true;
 
 				// If the command is viable then enable the buttons.
-				if (commandViable) {
-
+				if (commandViable)
+				{
 					// Show the command everywhere (affects both context menu and task pane).
 					// ref: http://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ICommands~SetCommandState.html
-					shellFrame.Commands.SetCommandState( g_MyCommandId, CommandLocation_All, CommandState_Active );
-
+					shellFrame.Commands.SetCommandState( commandId, CommandLocation_All, CommandState_Active );
 				}
-				else {
-
+				else
+				{
 					// Hide the command everywhere (affects both context menu and task pane).
 					// ref: http://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ICommands~SetCommandState.html
-					shellFrame.Commands.SetCommandState( g_MyCommandId, CommandLocation_All, CommandState_Hidden );
-
+					shellFrame.Commands.SetCommandState( commandId, CommandLocation_All, CommandState_Hidden );
 				}
-
 			} );
 	};
 }
