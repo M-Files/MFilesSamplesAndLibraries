028db1888471879828824e19b2e55336c469f8fe
CraigHawker <craig.hawker@gmail.com>
2017-07-25 17:08:28 +0100

    Added a sample of using displaying a persistent web page within a tab.

---
+++ Samples/UIX Applications/DisplayPersistentWebPageInTab/Readme.md
@@ -0,0 +1,5 @@
+# Persistent Web Page
+
+This application registers a [ShellUI module](http://developer.m-files.com/Frameworks/User-Interface-Extensibility-Framework/Modules/#shellui) that creates a persistent web page and shows the content within a tab on the right-hand-side of the interface.  The navigation within the tab is persisted even as the user interacts with M-Files (e.g. by navigating into a view).
+
+More information is available within the [M-Files Developer Portal](http://developer.m-files.com/Samples-And-Libraries/Samples/User-Interface-Extensibility-Framework/Display-Persistent-Web-Page-In-Tab/).
---
+++ Samples/UIX Applications/DisplayPersistentWebPageInTab/main.js
@@ -0,0 +1,79 @@
+// NOTE! This code is for demonstration purposes only and does not contain any kind of
+// 		 error handling. MUST be revised before using in production.
+//		 Authored by: Craig Hawker / M-Files
+
+"use strict";
+
+// A handle to the persistent browser content that will be used across all shell frames.
+var persistentContentHandle = null;
+
+function OnNewShellUI( shellUI )
+{
+	/// <summary>Executed by the UIX when a ShellUI module is started.</summary>
+	/// <param name="shellUI" type="MFiles.ShellUI">The shell UI object which was created.</param>
+ 
+	// This is the start point of a ShellUI module.
+ 
+	// Register to be notified when the shell ui is started.
+	shellUI.Events.Register(
+		Event_Started,
+		getShellUIStartedHandler(shellUI) );
+ 
+	// Register to be notified when a new shell frame (Event_NewShellFrame) is created.
+	shellUI.Events.Register(
+		Event_NewShellFrame,
+		handleNewShellFrame );
+}
+
+function getShellUIStartedHandler(shellUI)
+{
+	/// <summary>Returns a function which handles the OnStarted event for an IShellUI.</summary>
+
+	return function()
+	{
+		// The shell UI is now started and can be used.
+
+		// Create the persistent browser window (but do not show it).
+		persistentContentHandle = shellUI.CreatePersistentBrowserContent(
+			"http://developer.m-files.com",
+			{
+				defaultvisibility: false
+			} );
+	}
+}
+
+function handleNewShellFrame(shellFrame)
+{
+	/// <summary>Handles the OnNewShellFrame event for an IShellUI.</summary>
+	/// <param name="shellFrame" type="MFiles.ShellFrame">The shell frame object which was created.</param>
+ 
+	// The shell frame was created but it cannot be used yet.
+	// The following line would throw an exception ("The object cannot be accessed, because it is not ready."):
+	// shellFrame.ShowMessage("A shell frame was created");
+ 
+	// Register to be notified when the shell frame is started.
+	// This time pass a reference to the function to call when the event is fired.
+	shellFrame.Events.Register(
+		Event_Started,
+		getShellFrameStartedHandler( shellFrame) );
+}
+ 
+function getShellFrameStartedHandler(shellFrame)
+{
+	/// <summary>Returns a function which handles the OnStarted event for an IShellFrame.</summary>
+ 
+	return function() {
+		// The shell frame is now started and can be used.
+		
+		// Create a new tab for the content to be shown in.
+		// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~IShellPaneContainer~AddTab.html
+		// Tab ids ref: https://www.m-files.com/UI_Extensibility_Framework/#SidePaneTabs.html
+		var tab = shellFrame.RightPane.AddTab( "myPersistentContent", "Intranet", "_last" );
+
+		// Load the content into the tab.
+		tab.ShowPersistentContent( persistentContentHandle );
+
+		// Show the tab.
+		tab.visible = true;
+	}
+}
