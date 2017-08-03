4dbaae714c5859a083c1c75e03327d6f50fecabf
CraigHawker <craig.hawker@gmail.com>
2017-08-03 11:04:57 +0100

    Formatting of js files.

--- Samples/UIX Applications/Commands/main.js
+++ Samples/UIX Applications/Commands/main.js
@@ -5,42 +5,42 @@
 
 "use strict";
 
-function OnNewShellUI( shellUI )
+function OnNewShellUI(shellUI)
 {
 	/// <summary>Executed by the UIX when a ShellUI module is started.</summary>
 	/// <param name="shellUI" type="MFiles.ShellUI">The shell UI object which was created.</param>
- 
+
 	// This is the start point of a ShellUI module.
- 
+
 	// Register to be notified when a new shell frame (Event_NewShellFrame) is created.
 	shellUI.Events.Register(
 		Event_NewShellFrame,
 		handleNewShellFrame );
 }
- 
+
 function handleNewShellFrame(shellFrame)
 {
 	/// <summary>Handles the OnNewShellFrame event for an IShellUI.</summary>
 	/// <param name="shellFrame" type="MFiles.ShellFrame">The shell frame object which was created.</param>
- 
+
 	// The shell frame was created but it cannot be used yet.
 	// The following line would throw an exception ("The object cannot be accessed, because it is not ready."):
 	// shellFrame.ShowMessage("A shell frame was created");
- 
+
 	// Register to be notified when the shell frame is started.
 	shellFrame.Events.Register(
 		Event_Started,
-		getShellFrameStartedHandler(shellFrame));
+		getShellFrameStartedHandler( shellFrame ) );
 }
- 
+
 function getShellFrameStartedHandler(shellFrame)
 {
 	/// <summary>Returns a function which handles the OnStarted event for an IShellFrame.</summary>
- 
+
 	// The shell frame is now started and can be used.
 
-	return function() {
-
+	return function()
+	{
 		// Create a command (button).  Note that it is not yet visible.
 		// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ICommands~CreateCustomCommand.html
 		var commandOneId = shellFrame.Commands.CreateCustomCommand( "My First Command" );
@@ -58,11 +58,13 @@ function getShellFrameStartedHandler(shellFrame)
 
 		// Add the first and second commands to the task area.
 		// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ITaskPane~AddCustomCommandToGroup.html
-		try {
+		try
+		{
 			shellFrame.TaskPane.AddCustomCommandToGroup( commandOneId, TaskPaneGroup_Main, 1 );
 			shellFrame.TaskPane.AddCustomCommandToGroup( commandTwoId, TaskPaneGroup_Main, 1 );
 		}
-		catch (e) {
+		catch (e)
+		{
 			// This will except if the task pane is not available (e.g. in a History view).
 		}
 
@@ -75,24 +77,33 @@ function getShellFrameStartedHandler(shellFrame)
 		// Note: this will fire for ALL custom commands, so we need to filter out others.
 		shellFrame.Commands.Events.Register(
 			Event_CustomCommand,
-			function(commandId) {
+			function(commandId)
+			{
 				// Branch depending on the Id of the command that was clicked.
-				switch (commandId) {
+				switch (commandId)
+				{
+
 					case commandOneId:
+
 						// Our first command was clicked.
-						shellFrame.Commands.SetCommandState(commandTwoId,
+						shellFrame.Commands.SetCommandState(
+							commandTwoId,
 							CommandLocation_All,
-							CommandState_Active);
+							CommandState_Active );
+
 						break;
+
 					case commandTwoId:
+
 						// Our second command was clicked.
-						shellFrame.Commands.SetCommandState(commandTwoId,
+						shellFrame.Commands.SetCommandState(
+							commandTwoId,
 							CommandLocation_All,
-							CommandState_Inactive);
+							CommandState_Inactive );
+
 						break;
+
 				}
 			} );
-
 	}
-
 }
--- Samples/UIX Applications/HelloWorld/main.js
+++ Samples/UIX Applications/HelloWorld/main.js
@@ -4,40 +4,41 @@
 
 "use strict";
 
-function OnNewShellUI( shellUI )
+function OnNewShellUI(shellUI)
 {
 	/// <summary>Executed by the UIX when a ShellUI module is started.</summary>
 	/// <param name="shellUI" type="MFiles.ShellUI">The shell UI object which was created.</param>
- 
+
 	// This is the start point of a ShellUI module.
- 
+
 	// Register to be notified when a new shell frame (Event_NewShellFrame) is created.
 	shellUI.Events.Register(
 		Event_NewShellFrame,
 		handleNewShellFrame );
 }
- 
+
 function handleNewShellFrame(shellFrame)
 {
 	/// <summary>Handles the OnNewShellFrame event for an IShellUI.</summary>
 	/// <param name="shellFrame" type="MFiles.ShellFrame">The shell frame object which was created.</param>
- 
+
 	// The shell frame was created but it cannot be used yet.
 	// The following line would throw an exception ("The object cannot be accessed, because it is not ready."):
 	// shellFrame.ShowMessage("A shell frame was created");
- 
+
 	// Register to be notified when the shell frame is started.
 	// This time pass a reference to the function to call when the event is fired.
 	shellFrame.Events.Register(
 		Event_Started,
-		getShellFrameStartedHandler( shellFrame) );
+		getShellFrameStartedHandler( shellFrame ) );
 }
- 
+
 function getShellFrameStartedHandler(shellFrame)
 {
 	/// <summary>Returns a function which handles the OnStarted event for an IShellFrame.</summary>
- 
-	return function() {
+
+	return function()
+	{
 		// The shell frame is now started and can be used.
 		shellFrame.ShowMessage( "A shell frame is available for use." );
 	}
