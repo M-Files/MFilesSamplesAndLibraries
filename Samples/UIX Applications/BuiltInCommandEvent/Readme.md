0bb7b5044ec0c060bb7b1f7bf38acdc272cbc53e
CraigHawker <craig.hawker@gmail.com>
2017-12-19 16:47:22 +0000

    Updated BuiltInCommands sample to additionally show how to execute a built-in command (e.g. "Add document from scanner") from a UIX application.

--- Samples/UIX Applications/BuiltInCommandEvent/Readme.md
+++ Samples/UIX Applications/BuiltInCommandEvent/Readme.md
@@ -1,6 +1,6 @@
 # Reacting to built-in commands
 
-This application registers a [ShellUI module](http://developer.m-files.com/Frameworks/User-Interface-Extensibility-Framework/Modules/#shellui) that reacts when built-in commands are executed.
+This application registers a [ShellUI module](http://developer.m-files.com/Frameworks/User-Interface-Extensibility-Framework/Modules/#shellui) that reacts when built-in commands are executed.  The module also registers a command (button) into the left-hand task pane, which executes a built-in command ("Add document from scanner") when clicked.
 
 More information is available within the [M-Files Developer Portal](http://developer.m-files.com/Samples-And-Libraries/Samples/User-Interface-Extensibility-Framework/BuiltInCommand-Event/).
 
--- Samples/UIX Applications/BuiltInCommandEvent/main.js
+++ Samples/UIX Applications/BuiltInCommandEvent/main.js
@@ -8,10 +8,22 @@ function OnNewShellUI(shellUI)
 	/// <summary>The entry point of ShellUI module.</summary>
 	/// <param name="shellUI" type="MFiles.ShellUI">The new shell UI object.</param> 
 
+	// Note: We want to hook into the built-in commands being executed in any shell frame,
+	// but we want our command to only be used in NORMAL shell frames (as the task pane etc. is not available in others).
+
 	// Register to be notified when a new shell frame is created.
+	// This will be used to hook into the built-in commands whereever they are executed.
 	shellUI.Events.Register(
 		Event_NewShellFrame,
-		handleNewShellFrame );
+		handleNewShellFrame );	
+
+	// Register to be notified when a new normal shell frame (Event_NewNormalShellFrame) is created.
+	// We use Event_NewNormalShellFrame rather than Event_NewShellFrame as this won't fire for history (etc.) dialogs.
+	// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#Event_NewNormalShellFrame.html
+	// This will be used to add the button that executes a built-in command to the task pane.
+	shellUI.Events.Register(
+		Event_NewNormalShellFrame,
+		handleNewNormalShellFrame );
 }
 
 function handleNewShellFrame(shellFrame)
@@ -57,6 +69,57 @@ function getShellFrameStartedHandler(shellFrame)
 				// Although default value is true when nothing is returned.
 				// Return false to cancel the standard command execution.
 				return true;
+			} );
+	};
+}
+
+function handleNewNormalShellFrame(shellFrame)
+{
+	/// <summary>Handles the OnNewNormalShellFrame event for an IShellUI.</summary>
+	/// <param name="shellFrame" type="MFiles.ShellFrame">The shell frame object which was created.</param>
+
+	// The shell frame was created but it cannot be used yet.
+	// The following line would throw an exception ("The object cannot be accessed, because it is not ready."):
+	// shellFrame.ShowMessage("A shell frame was created");
+
+	// Register to be notified when the shell frame is started.
+	shellFrame.Events.Register(
+		Event_Started,
+		getNormalShellFrameStartedHandler(shellFrame) );
+}
+
+
+function getNormalShellFrameStartedHandler(shellFrame)
+{
+	/// <summary>Gets a function to handle the Started event for the normal shell frame.</summary>
+	/// <param name="shellFrame" type="MFiles.ShellFrame">The current shell frame object.</param> 
+	/// <returns type="MFiles.Events.OnStarted">The event handler.</returns>
+
+	// Return the handler function for Started event.
+	return function()
+	{
+		// Create a command (button).  Note that it is not yet visible.
+		// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ICommands~CreateCustomCommand.html
+		var documentFromScannerCommand = shellFrame.Commands.CreateCustomCommand( "Add document from scanner" );
+
+		// Add the command to the task area.
+		// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ITaskPane~AddCustomCommandToGroup.html
+		shellFrame.TaskPane.AddCustomCommandToGroup( documentFromScannerCommand, TaskPaneGroup_Main, 1 );
+
+		// Register to be notified when a custom command is clicked.
+		// Note: this will fire for ALL custom commands, so we need to filter out others.
+		shellFrame.Commands.Events.Register(
+			Event_CustomCommand,
+			function(commandId)
+			{
+				// Ignore other commands.
+				if (commandId != documentFromScannerCommand)
+					return;
+
+				// Execute a built-in command.
+				// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~ICommands~ExecuteCommand.html
+				// ref: https://www.m-files.com/UI_Extensibility_Framework/index.html#MFClientScript~BuiltinCommand.html
+				shellFrame.Commands.ExecuteCommand( BuiltinCommand_AddDocumentFromScanner, null );
 			} );
 	};
 }
