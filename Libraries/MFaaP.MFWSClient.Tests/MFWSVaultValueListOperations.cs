32cacd74d3b11592ea0ee730133aceab54672dd6
CraigHawker <craig.hawker@gmail.com>
2018-07-16 17:01:37 +0100

    Added methods and tests for value list alias to id resolution.

--- Libraries/MFaaP.MFWSClient.Tests/MFWSVaultValueListOperations.cs
+++ Libraries/MFaaP.MFWSClient.Tests/MFWSVaultValueListOperations.cs
@@ -11,6 +11,94 @@ namespace MFaaP.MFWSClient.Tests
 	public class MFWSVaultValueListOperations
 	{
 
+		#region Value list alias to ID resolution
+
+		/// <summary>
+		/// Ensures that a call to <see cref="MFaaP.MFWSClient.MFWSVaultValueListOperations.GetValueListIDByAliasAsync"/>
+		/// requests the correct resource address with the correct method.
+		/// </summary>
+		[TestMethod]
+		public async Task GetValueListIDByAliasAsync()
+		{
+			// Create our test runner.
+			var runner = new RestApiTestRunner<Dictionary<string, int>>(Method.POST, "/REST/structure/valuelists/itemidbyalias.aspx");
+
+			// Set up the expected body.
+			var body = new JsonArray { "hello world" };
+			runner.SetExpectedRequestBody(body);
+
+			// Execute.
+			await runner.MFWSClient.ValueListOperations.GetValueListIDByAliasAsync("hello world");
+
+			// Verify.
+			runner.Verify();
+		}
+
+		/// <summary>
+		/// Ensures that a call to <see cref="MFaaP.MFWSClient.MFWSVaultValueListOperations.GetValueListIDsByAliasesAsync"/>
+		/// requests the correct resource address with the correct method.
+		/// </summary>
+		[TestMethod]
+		public async Task GetValueListIDsByAliasesAsync()
+		{
+			// Create our test runner.
+			var runner = new RestApiTestRunner<Dictionary<string, int>>(Method.POST, "/REST/structure/valuelists/itemidbyalias.aspx");
+
+			// Set up the expected body.
+			var body = new JsonArray { "hello", "world", "third option" };
+			runner.SetExpectedRequestBody(body);
+
+			// Execute.
+			await runner.MFWSClient.ValueListOperations.GetValueListIDsByAliasesAsync(aliases: new string[] { "hello", "world", "third option" });
+
+			// Verify.
+			runner.Verify();
+		}
+
+		/// <summary>
+		/// Ensures that a call to <see cref="MFaaP.MFWSClient.MFWSVaultValueListOperations.GetValueListIDsByAliases"/>
+		/// requests the correct resource address with the correct method.
+		/// </summary>
+		[TestMethod]
+		public void GetValueListIDsByAliases()
+		{
+			// Create our test runner.
+			var runner = new RestApiTestRunner<Dictionary<string, int>>(Method.POST, "/REST/structure/valuelists/itemidbyalias.aspx");
+
+			// Set up the expected body.
+			var body = new JsonArray { "hello", "world", "third option" };
+			runner.SetExpectedRequestBody(body);
+
+			// Execute.
+			runner.MFWSClient.ValueListOperations.GetValueListIDsByAliases(aliases: new string[] { "hello", "world", "third option" });
+
+			// Verify.
+			runner.Verify();
+		}
+
+		/// <summary>
+		/// Ensures that a call to <see cref="MFaaP.MFWSClient.MFWSVaultValueListOperations.GetValueListIDByAlias"/>
+		/// requests the correct resource address with the correct method.
+		/// </summary>
+		[TestMethod]
+		public void GetValueListIDByAlias()
+		{
+			// Create our test runner.
+			var runner = new RestApiTestRunner<Dictionary<string, int>>(Method.POST, "/REST/structure/valuelists/itemidbyalias.aspx");
+
+			// Set up the expected body.
+			var body = new JsonArray { "hello world" };
+			runner.SetExpectedRequestBody(body);
+
+			// Execute.
+			runner.MFWSClient.ValueListOperations.GetValueListIDByAlias("hello world");
+
+			// Verify.
+			runner.Verify();
+		}
+
+		#endregion
+
 		#region GetValueLists
 
 		/// <summary>
