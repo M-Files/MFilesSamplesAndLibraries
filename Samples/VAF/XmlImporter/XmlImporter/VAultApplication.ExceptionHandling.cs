1bbcb31a569379b68b39b69e8edc493fc275fb3b
Craig Hawker <craig.hawker@gmail.com>
2019-01-17 10:24:25 +0000

    Resolved missing exception handling code.

--- Samples/VAF/XmlImporter/XmlImporter/VAultApplication.ExceptionHandling.cs
+++ Samples/VAF/XmlImporter/XmlImporter/VAultApplication.ExceptionHandling.cs
@@ -1,6 +1,90 @@
-﻿namespace XmlImporter
+﻿using System;
+using System.IO;
+using MFiles.VAF.Common;
+
+namespace XmlImporter
 {
 	public partial class VaultApplication
 	{
+		/// <summary>
+		/// Handles an exception according to the <see cref="ImportInstruction.ImportExceptionHandlingStrategy"/>.
+		/// </summary>
+		/// <param name="importInstruction">The import instruction being processed.</param>
+		/// <param name="fileBeingProcessed">The file which was being processed when the exception occurred.</param>
+		/// <param name="importException">The exception raised.</param>
+		public void HandleImportException(
+			ImportInstruction importInstruction,
+			FileInfo fileBeingProcessed,
+			Exception importException)
+		{
+			// Sanity.
+			if (null == importInstruction)
+				throw new ArgumentNullException(nameof(importInstruction));
+			if (null == fileBeingProcessed)
+				throw new ArgumentNullException(nameof(fileBeingProcessed));
+			if (null == importException)
+				throw new ArgumentNullException(nameof(importException));
+#if DEBUG
+			System.Diagnostics.Debugger.Launch();
+#endif
+			// Report the error.
+			SysUtils.ReportErrorMessageToEventLog($"Exception importing {fileBeingProcessed.FullName}", importException);
+
+			// What is the exception handling strategy?
+			try
+			{
+				switch (importInstruction.ImportExceptionHandlingStrategy)
+				{
+					case ImportExceptionHandlingStrategy.Delete:
+						{
+							// Delete the file.
+							fileBeingProcessed.Delete();
+							break;
+						}
+					case ImportExceptionHandlingStrategy.MoveToSpecificFolder:
+						{
+							// Get a reference to the folder to move the file to.
+							var targetDirectoryInfo = new DirectoryInfo(importInstruction.ExceptionFolderName);
+
+							// Create it if it does not exist.
+							if (false == targetDirectoryInfo.Exists)
+								targetDirectoryInfo.Create();
+
+							// Move the file there.
+							fileBeingProcessed.MoveTo(
+								System.IO.Path.Combine(targetDirectoryInfo.FullName, fileBeingProcessed.Name)
+							);
+							break;
+						}
+					case ImportExceptionHandlingStrategy.MoveToSubFolder:
+						{
+							// Get a reference to the folder to move the file to.
+							var targetDirectoryInfo = new DirectoryInfo(System.IO.Path.Combine(
+								fileBeingProcessed.DirectoryName,
+								importInstruction.ExceptionSubFolderName));
+
+							// Create it if it does not exist.
+							if (false == targetDirectoryInfo.Exists)
+								targetDirectoryInfo.Create();
+
+							// Move the file there.
+							fileBeingProcessed.MoveTo(
+								System.IO.Path.Combine(targetDirectoryInfo.FullName, fileBeingProcessed.Name)
+							);
+							break;
+						}
+					default:
+						throw new InvalidOperationException(
+							$"Unhandled import exception handling strategy: {importInstruction.ImportExceptionHandlingStrategy}");
+				}
+			}
+			catch (Exception e)
+			{
+				// Report the exception.
+				SysUtils.ReportErrorMessageToEventLog(
+					"An exception was thrown whilst trying to process the exception handling strategy.",
+					e);
+			}
+		}
 	}
 }
